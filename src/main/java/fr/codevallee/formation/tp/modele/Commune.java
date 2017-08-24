4d06e6de47628367601ec6bef78936e334764757
Formation 1 <eliole@hotmail.fr>
2017-08-24 15:27:18 +0200

    Création du TP 5 sur une nouvelle branche

---
+++ src/main/java/fr/codevallee/formation/tp/modele/Commune.java
@@ -0,0 +1,65 @@
+package fr.codevallee.formation.tp.modele;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Commune {
+	
+	@Id
+	@GeneratedValue( strategy = GenerationType.AUTO )
+	private long id;
+	
+	@Column(length = 40)
+	private String nom;
+	
+	@OneToOne
+	private Maire maire;
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * @return the maire
+	 */
+	public Maire getMaire() {
+		return maire;
+	}
+
+	/**
+	 * @param maire the maire to set
+	 */
+	public void setMaire(Maire maire) {
+		this.maire = maire;
+	}
+
+}
---
+++ src/main/java/fr/codevallee/formation/tp/modele/Maire.java
@@ -0,0 +1,48 @@
+package fr.codevallee.formation.tp.modele;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Maire {
+
+	@Id
+	@GeneratedValue( strategy = GenerationType.AUTO )
+	private long id;
+	
+	@Column(length = 40)
+	private String nom;
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+}
